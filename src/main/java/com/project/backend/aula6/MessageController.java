96fae34e2ff7be064c7ec6b5480d12da33406472
juo <juliano.almeida17@gmail.com>
2026-04-28 20:19:59 -0300

    first commit

---
+++ src/main/java/com/project/backend/aula6/MessageController.java
@@ -0,0 +1,52 @@
+package com.project.backend.aula6;
+
+import org.apache.logging.log4j.message.Message;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    private List<String> messages = new ArrayList<>();
+
+    @GetMapping
+    public List<String> list() {
+        return messages;
+    }
+
+    @PostMapping
+    public String add(@RequestBody String message) {
+        messages.add(message);
+        return "Mensagem adicionada com sucesso";
+    }
+    /*
+    @PutMapping("/{index}")
+    public String update(@RequestBody String message, @PathVariable int index) {
+        if (index >= 0 && index < messages.size()) {
+            messages.set(index, message);
+            return "Mensagem atualizada com sucesso!";
+        }
+        return "Índice inválido ou array vazio!";
+    }
+    */
+    @PutMapping("/{index}")
+    public String update(@RequestBody String message, @PathVariable int index) {
+        if (index >= 0 && index < messages.size()) {
+            messages.set(index, message);
+            return "Mensagem atualizada com sucesso!";
+        }
+        return "Índice inválido ou array vazio!";
+    }
+
+    @DeleteMapping("/{index}")
+    public String remover(@PathVariable int index) {
+        if (index >= 0 && index < messages.size()) {
+            messages.remove(index);
+            return "Mensagem removida!";
+        }
+        return "Índice inválido ou array vazio!";
+    }
+}
